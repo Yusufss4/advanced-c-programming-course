91251631f872561b64b159e1a647098899722ebe
Yusuf Savaş <yusufsss4@gmail.com>
2022-08-15 22:46:07 +0300

    Add Chapter 17 example 3 dynamic loading

--- Chapter 17 - Static Libraries and Shared Objects/17-example-3/main.c
+++ Chapter 17 - Static Libraries and Shared Objects/17-example-3/main.c
@@ -1,8 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <myLib.h>
+#include <dlfcn.h>
 
-void main()
-{
-	fun();
+int main(void) {
+	void *handle = NULL;
+	double (*cosine)(double) = NULL; // pointer function. 
+	char *error = NULL;
+
+	handle = dlopen("/lib/x86_64-linux-gnu/lib",RTLD_LAZY);
+
+	if (!handle) {
+		fputs(dlerror(),stderr);
+		exit(1);
+	}
+
+	dlerror(); // reset the previous error.
+
+	cosine = dlsym(handle,"cos"); // laod the function into the cosine.
+
+	if((error = dlerror()) != NULL) {
+		fputs(error,stderr); 
+		exit(1); // if there is an error with loading exit.
+	}
+
+	printf("%f\n", (*cosine)(2.0)); // call the function pointer.
+
+	dlclose(handle); // unload the function.
+
+	return 0;
 }
-
